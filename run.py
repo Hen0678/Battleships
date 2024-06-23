f3cf06e0e0996df11087032031ccd9c4dec34992
Andy Cohen <andycohen32@gmail.com>
2024-06-23 16:56:28 +0000

    Finished adding conditions to game when all turns have been used

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ guess_board = [[' '] * 8 for x in range(8)]
 letters_to_numbers = {'a': 0, 'b': 1, 'c':2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 
 def print_board(board):
-    print('a b c d e f g h')
-    print('---------------')
+    print('  a b c d e f g h')
+    print('-----------------')
     # Iterates 1 through to 8 and joins on a "|" 
     row_number = 1
     for row in board:
@@ -27,15 +27,15 @@ def make_ships(board):
 
 # Will ask ther user the row and column for their guess
 def ship_location():
-    row = input('Enter a row number (1-8)')
+    row = input('Enter a row number (1-8): ')
     # While loop if a number other than 1-8 is entered
     while row not in '12345678':
         print('Error - please enter a number (1-8)')
-        row = input('Enter a row number (1-8)')
-    column = input('Enter a column letter (a-h)')
+        row = input('Enter a row number (1-8): ')
+    column = input('Enter a column letter (a-h): ')
     while column not in 'abcdefgh':
         print('Error - please enter a letter (a-h)')
-        column = input('Enter a column letter (a-h)')
+        column = input('Enter a column letter (a-h): ')
     return int(row) - 1, letters_to_numbers[column]
 
 # Will count every time a ship is hit
@@ -54,7 +54,7 @@ turns = 10
 #print_board(player_board)
 #print_board(guess_board)
 while turns > 0:
-    print('Let battle commence!')
+    print('Get ready to fire')
     print_board(guess_board)
     row, column = ship_location()
     # Will check to see if this guess has already been made
@@ -71,5 +71,14 @@ while turns > 0:
         # Add an '-' where the incorrect guess was made
         guess_board[row][column] = '-'
         turns -= 1
-
+    # Where all 5 computer ships have been hit
+    if count_ships_hit(guess_board) == 5:
+        print('You have successfully destroyed the enemy fleet')
+        break
+    # Tells the user how many turns they have left
+    print('You have ' + str(turns) + ' missles remaining')
+    # Where all turns have been used up
+    if turns == 0:
+        print('You are out of missles - game over')
+        break
 
