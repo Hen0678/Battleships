de47a55b50bdbfc79e6f34e8b51df72ec0b21757
Andy Cohen <andycohen32@gmail.com>
2024-06-23 14:34:18 +0000

    Added random import randint and added for loop to make random generated ships

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+from random import randint
+
 # Creates an 8 x 8 board that will hold the player ships
 player_board = [[''] * 8 for x in range(8)]
 # Creates an 8 x 8 board for player guesses
 guess_board = [[''] * 8 for x in range(8)]
-
+print(player_board)
+print(guess_board)
 
 # A way to convert letters to numbers
 letters_to_numbers = {'a': 0, 'b': 1, 'c':2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
@@ -16,7 +19,11 @@ def print_board(board):
         print("%d|%s|" %  (row_number, "|".join(row)))
         row_number += 1
 
+# Ships will be generated randomly 
 def make_ships():
+    for ship in range(5):
+        ship row, ship column = randint(0,7), randint(0,7)
+
 
 # Will ask ther user the row and column for their guess
 def ship_location():
