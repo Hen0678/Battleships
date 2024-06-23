8428a6c8e4c6dea87d582be059d96a2ffafa415f
Andy Cohen <andycohen32@gmail.com>
2024-06-23 14:06:52 +0000

    Added board and letters to numbers variables and initial commit

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Creates an 8 x 8 board that will hold the player ships
+player_board = [[''] * 8 for x in range(8)]
+# Creates an 8 x 8 board for player guesses
+guess_board = [[''] * 8 for x in range(8)]
+
+
+# A way to convert letters to numbers
+letters_to_numbers = {'a': 0, 'b': 1, 'c':2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
